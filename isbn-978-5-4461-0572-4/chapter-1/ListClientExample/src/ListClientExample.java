4875edbe83629930be4d59994f2e8d8be3340816
Sergey Podrezov <sergey.podrezov@gmail.com>
2022-04-26 19:02:46 +0600

    Поменять реализацию LinkedList на ArrayList

---
+++ isbn-978-5-4461-0572-4/chapter-1/ListClientExample/src/ListClientExample.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListClientExample {
+    private List list;
+
+    public ListClientExample() {
+        list = new ArrayList();
+    }
+
+    private List getList() {
+        return list;
+    }
+
+    public static void main(String[] args) {
+        ListClientExample lce = new ListClientExample();
+        List list = lce.getList();
+        System.out.println(list);
+    }
+}
